003199b1f56f8046162e91eb839ca4a3b93e401f
Eoin Harkin <eharky@gmail.com>
2023-02-06 01:40:35 +0000

    redux state mangment in place

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import Home from 'pages/home';
+import Login from "pages/login"
+import Profile from "pages/profile"
+import './App.css';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme.js';
+
+function App() {
+
+  const mode = useSelector((state)=> state.mode)
+  const theme = useMemo (()=> createTheme(themeSettings(mode)),[mode])
+
+
+
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Login/>}> </Route>
+          <Route path="/home" element={<Home/>}> </Route>
+          <Route path="/profile/:userId" element={<Profile/>}> </Route>
+        </Routes>
+      </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
